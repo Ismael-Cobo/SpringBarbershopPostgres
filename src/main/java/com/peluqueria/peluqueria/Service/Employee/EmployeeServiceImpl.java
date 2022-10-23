030c4a4c70384b25377963384e09252a3e12851f
Ismael Cobo <midismael@gmail.com>
2022-10-23 15:12:16 +0200

    fix: delete hairassistance

--- src/main/java/com/peluqueria/peluqueria/Service/Employee/EmployeeServiceImpl.java
+++ src/main/java/com/peluqueria/peluqueria/Service/Employee/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.peluqueria.peluqueria.Service.Employee;
 
+import com.peluqueria.peluqueria.Entity.Appointment;
 import com.peluqueria.peluqueria.Entity.Employee;
 import com.peluqueria.peluqueria.Repository.EmployeeRepository;
+import com.peluqueria.peluqueria.Service.Appointment.AppointmentService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,8 +16,14 @@ public class EmployeeServiceImpl implements EmployeeService {
     
     private final EmployeeRepository employeeRepository;
     
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+    private final AppointmentService appointmentService;
+    
+    public EmployeeServiceImpl(
+            EmployeeRepository employeeRepository,
+            AppointmentService appointmentService
+            ) {
         this.employeeRepository = employeeRepository;
+        this.appointmentService = appointmentService;
     }
     
     @Override
@@ -40,6 +48,10 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public boolean deleteById(Long id) {
         if(id == null) return false;
+        
+        List<Appointment> appointments = appointmentService.findAllByHairAssistanceId(id);
+        
+        appointments.forEach(a -> a.setHairAssistance(null));
         
         employeeRepository.deleteById(id);
         return true;
